2fc893bd50896318e8da0650fa79b28fabfe9a01
vananh.nguyen <vananh211291@gmail.com>
2018-07-21 16:35:18 +0700

    init commit

---
+++ test/test.js
@@ -0,0 +1,24 @@
+var assert = require('assert');
+var should = require('chai').should();
+
+describe('Gamuko', function(){
+    it('4 trang doc la thang',  function () {
+        
+    });
+
+    it('4 trang ngang la thang',  function () {
+       
+    });
+
+    it('4 trang cheo la thang',  function () {
+        
+    });
+
+    it('5 chan la thang',  function () {
+        
+    });
+
+
+
+
+});
